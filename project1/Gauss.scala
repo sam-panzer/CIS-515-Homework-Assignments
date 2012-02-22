6a70eef78902dbad74790cad41b8c7ab0dc2834e
Sam Panzer <panzers@seas.upenn.edu>
2012-02-22 02:27:04 -0500

    Added back-substitution

--- project1/Gauss.scala
+++ project1/Gauss.scala
@@ -1,5 +1,7 @@
 object Gauss {
     type Matrix = Array[Array[Double]]
+
+    // Determines the index of the highest value
     def maxOf(l: Seq[Double]): Int = {
         val m = l.zipWithIndex
         m.reduceLeft((acc,x) => {
@@ -8,6 +10,8 @@ object Gauss {
             if (math.abs(p) > math.abs(ap)) x else acc
         })._2
     }
+
+    // Swaps rows a and b in matrix mat
     def swapRows(mat: Matrix, a: Int, b: Int) = {
         val start = math.min(a,b)
         val end = math.max(a,b)
@@ -15,6 +19,8 @@ object Gauss {
         mat(start) = mat(end)
         mat(end) = s
     }
+
+    // Gaussian elimination
     def elimination(mat: Matrix): Matrix = {
         val m = mat.clone
         for (c <- 0 until m.length) {
@@ -33,6 +39,9 @@ object Gauss {
         }
         m
     }
+
+    // Prints the matrix mat. Not the prettiest printer, but better than just
+    // writing to stdout
     def printMatrix(mat: Matrix) = {
         for (i <- 0 until mat.length) {
             print("[ ")
@@ -42,8 +51,20 @@ object Gauss {
             println(" ]")
         }
     }
-    def backSubstitute(mat: Matrix) = {
-        val m = mat.clone()
+
+    // Assumes that mat is a triangular matrix with a nonzero trace
+    def backSubstitute(mat: Matrix, dest: List[Double]): List[Double] = {
+        def dot(v1: Seq[Double], v2: Seq[Double]) = {
+          val w: Seq[(Double, Double)] = v1 zip v2
+          w.foldLeft (0.0) ((acc: Double, x: (Double,Double)) => {
+            val (p1, p2) = x
+            acc + p1 * p2
+          }) 
+        }
+        mat.foldRight (Nil: List[Double])((x: Array[Double],acc: List[Double]) =>{
+            val w = x.takeRight(acc.length + 1)  
+            ((dest.drop(acc.length)(0) - dot(w.drop(1), acc)) / w(0)) :: acc
+          })
     }
 }
 
