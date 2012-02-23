d244c49eef0422075b3c52a7cd70b1c9c132c469
Sam Panzer <panzers@seas.upenn.edu>
2012-02-22 20:55:06 -0500

    Added tridiagonal solving

---
+++ project1/Gauss.h
@@ -0,0 +1,223 @@
+#ifndef __GAUSS_H__
+#define __GAUSS_H__
+
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+const double tolerance = 1e-10;
+using std::cout;
+
+double round(double d) {
+  const double e = fabs(d);
+  if (e - int(e) < tolerance) {
+    return (d >= 0) ? int(d) : int(d) + 1;
+  }
+  return d;
+}
+
+double dot(double *v1, double *v2, unsigned int len) {
+  double d = 0;
+  for (unsigned int i = 0; i < len; i++) {
+    d += v1[i] * v2[i];
+  }
+  return d;
+}
+
+template<int m, int n>
+class Matrix {
+  private:
+    double mat [m][n];
+
+  public:
+    double rows() const;
+    double columns() const;
+
+    double * operator[](int index);
+    const double *operator[](int index) const;
+    template<int p> Matrix<m,p> operator*(const Matrix<n,p>) const;
+    Matrix<m,1> operator*(const double vec[n]) const;
+
+    void swapRows(unsigned int a, unsigned int b);
+    void gaussElim();
+
+    // id isn't available for non-square matrices!
+    static Matrix<m,n> id();
+
+    void printMatrix();
+    double *backSubstitute(double dest[m], double target[m]);
+    void luSolve(double dest[m], double target[m]);
+
+    Matrix();
+    Matrix(const double src[m][n]);
+    ~Matrix();
+};
+
+
+template<int m,int n>
+Matrix<m,n> Matrix<m,n>::id() {
+  Matrix<m,n> mat;
+  const int l = m < n ? m : n;
+  for (int i = 0; i < l; i++) {
+    mat[i][i] = 1;
+  }
+  return mat;
+}
+
+template<int m, int n>
+template<int p>
+Matrix<m,p> Matrix<m,n>::operator*(const Matrix<n,p> m2) const {
+  double res[m][p];
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < p; j++) {
+      for (int k = 0; k < n; k++) {
+        res[i][j] += mat[i][k] * m2[k][j];
+      }
+      res[i][j] = res[i][j];
+    }
+  }
+  return Matrix<m,p>(res);
+}
+
+template<int m, int n>
+Matrix<m,1> Matrix<m,n>::operator*(const double vec[n]) const {
+  double res[m][1];
+  for (int i = 0; i < m; i++) {
+    for (int k = 0; k < n; k++) {
+      res[i][0] += mat[i][k] * vec[k];
+    }
+    res[i][0] = res[i][0];
+  }
+  return Matrix<m,1>(res);
+}
+
+
+template<int m, int n>
+double Matrix<m,n>::rows() const {
+  return m;
+}
+template<int m, int n>
+double Matrix<m,n>::columns() const {
+  return n;
+}
+
+template<int m, int n>
+double* Matrix<m,n>::operator[](int index) {
+  if (index < 0 || index > m) {
+    return NULL;
+  }
+  return mat[index];
+}
+
+template<int m, int n>
+const double* Matrix<m,n>::operator[](int index) const {
+  if (index < 0 || index > m) {
+    return NULL;
+  }
+  return mat[index];
+}
+
+template<int m, int n>
+Matrix<m,n>::Matrix() {
+  memset(mat, 0, m* n * sizeof(double));
+}
+
+template<int m, int n>
+Matrix<m,n>::Matrix(const double src[m][n]) {
+  memcpy(mat, src, m* n * sizeof(double));
+}
+
+template<int m, int n>
+Matrix<m,n>::~Matrix() {
+
+}
+
+
+// Swaps rows a and b in matrix mat
+template<int m, int n>
+void Matrix<m,n>::swapRows(unsigned int a, unsigned int b) {
+  for (int i = 0; i < n; i++) {
+    double d = mat[a][i];
+    mat[a][i] = mat[b][i];
+    mat[b][i] = d;
+  }
+}
+// Gaussian elimination
+template<int m, int n>
+void Matrix<m,n>::gaussElim() {
+  for (int c = 0; c < n; c++) {
+    int iMax = 0;
+    double vMax = 0.0;
+    for (int r = c; r < n; r++) {
+      int v = fabs(mat[r][c]);
+      if(v > vMax) {
+        vMax = v;
+        iMax = r;
+      }
+    }
+    if (vMax == 0)
+      continue; 
+    swapRows(c, iMax);
+
+    for (int i = c + 1; i < m; i++) {
+      for (int j = c  + 1; j < n; j++) {
+          mat[i][j] -= mat[c][j] * (mat[i][c] / mat[c][c]);
+      }
+      mat[i][c] = 0;
+    }
+  }
+}
+
+// Prints the matrix mat. Not the prettiest printer, but better than just
+// writing to stdout
+template<int m, int n>
+void Matrix<m,n>::printMatrix() {
+  cout << std::setiosflags(std::ios :: left);
+  cout << std::setprecision(4);
+  for (int i = 0; i < m; i++) {
+    cout << "[ ";
+    for (int j = 0; j < n; j++) {
+      cout << std::setw(8) << round(mat[i][j]);
+    }
+    cout << " ]\n";
+  }
+}
+
+// Assumes that mat is a triangular matrix with a nonzero trace
+template<int m, int n>
+double *Matrix<m,n>::backSubstitute(double dest[m], double target[m]) {
+  for (int j = m - 1; j >= 0; j--) {
+    double d = dot(dest + (j + 1), mat[j] + (j + 1), m - (j + 1));
+    dest[j] = (target[j] - d) / mat[j][j];
+  }
+  return dest;
+}
+
+// Assumes that mat is a tridiagonal square matrix
+template<int m, int n>
+void Matrix<m,n>::luSolve(double dest[m], double target[m]) {
+  double w[m];
+  double z[m];
+
+  z[0] = mat[0][0 + 1] / mat[0][0];
+  for (int i = 1; i < m - 1; i++) {
+    z[i] = mat[i][i + 1] / (mat[i][i] - mat[i][i - 1] * z[i-1]);
+  }
+  z[m - 1] = mat[m - 1][m-1] - mat[m-1][m-2] * z[m-2];
+
+  w[0] = target[0] / mat[0][0];
+  for (int i = 1; i < m; i++) {
+    double num   = (target[i] - mat[i][i-1] * w[i-1]);
+    double denom = (mat[i][i] - mat[i][i-1] * z[i-1]);
+    w[i] =  num / denom;
+  }
+
+  dest[m - 1] = w[m - 1];
+  for (int i = m - 2; i >= 0; i--) {
+    dest[i] = w[i] - z[i] * dest[i + 1];
+  }
+}
+
+#endif
--- project1/Matrix.cpp
+++ project1/Matrix.cpp
@@ -5,9 +5,22 @@ using std::cout;
 int main() {
 
   double backing[4][4] = {{1.0,2.0,3.0,4.0},
-                        {5.0,6.0,8.0,8.0},
-                        {7.0,2.0,5.0,1.0},
-                        {1.0,2.0,0.0,6.0}};
+                          {5.0,6.0,8.0,8.0},
+                          {7.0,2.0,5.0,1.0},
+                          {1.0,2.0,0.0,6.0}};
+
+  double tridiagonal[10][10] = {
+    {1, 86, 0, 0, 0, 0, 0, 0, 0, 0},
+    {736842, 2, 26, 0, 0, 0, 0, 0, 0, 0},
+    {0, -248637, 3, 42, 0, 0, 0, 0, 0, 0},
+    {0, 0, 9, 4, 3.14159, 0, 0, 0, 0, 0},
+    {0, 0, 0, -32, 5, 20.25, 0, 0, 0, 0},
+    {0, 0, 0, 0, -0x32, 6, 12.137, 0, 0, 0},
+    {0, 0, 0, 0, 0, 032, 7, 107432.7, 0, 0},
+    {0, 0, 0, 0, 0, 0, 3.2, 8, -2643, 0},
+    {0, 0, 0, 0, 0, 0, 0, 63, 9, 4.6e-4},
+    {0, 0, 0, 0, 0, 0, 0, 0, -6291993, 10}
+  };
   Matrix<4,4> mat(backing);
   Matrix<4,4> id = Matrix<4,4>::id();
   id.printMatrix();
@@ -24,6 +37,29 @@ int main() {
   cout << "\nResult:\n";
   Matrix<4,1> m2 = mat * res;
   m2.printMatrix();
+
+  Matrix<10,10> tridiag(tridiagonal);
+
+  cout << "\nNow to test tridiagonal solving. Here's the GaussElim solution\n";
+  tridiag.printMatrix();
+  double resTen[10];
+  double targetTen[10] = {10,9,8,7,6,5,4,3,2,1};
+
+  tridiag.gaussElim();
+  tridiag.backSubstitute(resTen, targetTen);
+  cout << "Got back:\n";
+  (tridiag.id() * resTen).printMatrix();
+  cout << "\nResult:\n";
+  (tridiag * resTen).printMatrix();
+
+  // And using the tridiagonal solution...
+  cout << "\nAnd the tridiagonal solution\n";
+  tridiag = Matrix<10,10>(tridiagonal);
+  tridiag.luSolve(resTen, targetTen);
+  cout << "Got back:\n";
+  (tridiag.id() * resTen).printMatrix();
+  cout << "\nResult:\n";
+  (tridiag * resTen).printMatrix();
 
   return 0;
 }
