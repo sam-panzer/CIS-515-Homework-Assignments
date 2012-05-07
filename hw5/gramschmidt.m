bcbd399169b7753d14c310923f923cef933814d0
U-kshi\kevin <kevin@kshi.(none)>
2012-05-07 00:08:49 -0400

    gram schmidt

--- hw5/gramschmidt.m
+++ hw5/gramschmidt.m
@@ -1,19 +1,23 @@
-function [ V ] = gramschmidt( W )
+function [ E ] = gramschmidt( W )
 
 %W is input matrix whose rows are the vectors to be orthonormalized
-%V is output matrix whose rows are the orthonormalized vectors of W
+%E is output matrix whose rows are the orthonormalized vectors of W
 
 dimensions = size(W);
 
 V = zeros(dimensions(1), dimensions(2));
+E = zeros(dimensions(1), dimensions(2));
 
 for i=1:dimensions(1)
-    V(i,:) = W(i,:) / norm(W(i,:));
+    V(i,:) = W(i,:);
     for j=1:i-1
         
-        V(i,:) = V(i,:) - dot(V(i,:),V(j,:)) / norm(V(i,:)) * V(j,:);
+        V(i,:) = V(i,:) - dot(V(i,:),V(j,:)) / dot(V(j,:),V(j,:)) * V(j,:);
         
     end
     
+    V(i,:) = V(i,:);
+    
+    E(i,:) = V(i,:) / norm(V(i,:));
+    
 end
-
